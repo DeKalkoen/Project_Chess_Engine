8d2d1ef0668773ff06a6a0260631c53fb3c6655c
DeKalkoen <47044950+DeKalkoen@users.noreply.github.com>
2020-11-25 01:55:41 +0100

    castle fix

--- chess.js
+++ chess.js
@@ -58,11 +58,13 @@ class Chess {
             if (move.isCastle) {
                 if (move.toX == 6) {
                     let blackRookIndex = nextBoard.getPieceIndex(7, 7, BLACK)
+                    nextBoard.squares[7][7] = null;
                     nextBoard.blackPieces[blackRookIndex].x = 5
                     nextBoard.squares[7][5] = nextBoard.blackPieces[blackRookIndex]
                 }
                 else if (move.toX == 2) {
                     let blackRookIndex = nextBoard.getPieceIndex(0, 7, BLACK)
+                    nextBoard.squares[7][0] = null;
                     nextBoard.blackPieces[blackRookIndex].x = 3
                     nextBoard.squares[7][3] = nextBoard.blackPieces[blackRookIndex]
                 }
@@ -75,11 +77,13 @@ class Chess {
             if (move.isCastle) {
                 if (move.toX == 6) {
                     let whiteRookIndex = nextBoard.getPieceIndex(7, 0, WHITE)
+                    nextBoard.squares[0][7] = null;
                     nextBoard.whitePieces[whiteRookIndex].x = 5;
                     nextBoard.squares[0][5] = nextBoard.whitePieces[whiteRookIndex]
                 }
                 else if (move.toX == 2) {
                     let whiteRookIndex = nextBoard.getPieceIndex(0, 0, WHITE)
+                    nextBoard.squares[0][0] = null
                     nextBoard.whitePieces[whiteRookIndex].x = 3;
                     nextBoard.squares[0][3] = nextBoard.whitePieces[whiteRookIndex]
                 }
